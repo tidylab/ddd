1a4c2a344d2ffb6dfe13e431a98e8c9cb2acfc0e
harell <harel@loostiger.com>
2020-10-01 19:42:46 +1300

    do_pkgdown

--- tic.R
+++ tic.R
@@ -10,20 +10,18 @@
 library(tic, warn.conflicts = FALSE)
 source("./.app/tic/helpers.R")
 
+
+# Macros ------------------------------------------------------------------
+# if(is_master_branch()
+do_pkgdown(orphan = TRUE)
+
 # Stage: Before Script ----------------------------------------------------
 get_stage("before_script") %>%
     add_code_step(install_deps()) %>%
     add_code_step(try(devtools::uninstall(), silent = TRUE))
 
 # Stage: Script -----------------------------------------------------------
-if(is_master_branch() | is_hotfix_branch()){
-    get_stage("script") %>% build_steps() %>% test_suite_steps()
-} else if (is_develop_branch() | is_release_branch()){
-    get_stage("script") %>% build_steps() %>% test_suite_steps()
-
-} else if (is_feature_branch()){
-    get_stage("script") %>% test_suite_steps()
-}
+get_stage("script") %>% build_steps() %>% test_suite_steps()
 
 # Stage: After Success ----------------------------------------------------
 get_stage("after_success")
@@ -38,6 +36,7 @@ get_stage("before_deploy")
 # Stage: Deploy -----------------------------------------------------------
 get_stage("deploy") %>%
     publish_package_coverage()
+
 
 # Stage: After Deploy -----------------------------------------------------
 get_stage("after_deploy")
