f3d1c8a648ea0b0fd6c583fad061b3d806a30880
harell <harel@loostiger.com>
2020-10-03 12:36:05 +1300

    setup GitHub actions

---
+++ inst/scripts/GitHub-Actions.R
@@ -0,0 +1,7 @@
+# Setup -------------------------------------------------------------------
+remotes::install_github("maxheld83/ghactions")
+ghactions::use_ghactions(workflow = ghactions::website())
+
+# Build pkgdown site ------------------------------------------------------
+
+
