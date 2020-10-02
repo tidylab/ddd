84f18646a4521cd70ff9fa06ed40fea5ac64cbd2
harell <harel@loostiger.com>
2020-10-03 12:53:51 +1300

    run only on Linux without Rprofile

--- inst/scripts/GitHub-Actions.R
+++ inst/scripts/GitHub-Actions.R
@@ -1,6 +1,11 @@
 # Setup -------------------------------------------------------------------
-remotes::install_github("maxheld83/ghactions")
-ghactions::use_ghactions(workflow = ghactions::website())
+tic::use_ghactions_yml(
+    type = "linux-deploy",
+    write = TRUE,
+    quiet = FALSE
+)
+
+
 
 # Build pkgdown site ------------------------------------------------------
 
