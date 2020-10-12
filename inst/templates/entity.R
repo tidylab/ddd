347d30c445a7d594c42ba39f9fbddfe3ee0e0d48
harell <harel@loostiger.com>
2020-10-12 20:12:45 +1300

    entity template

---
+++ inst/templates/entity.R
@@ -0,0 +1,36 @@
+#' @title Entity Name
+#'
+#' @description TODO: Describe how this entity models unique things
+#'
+#'
+#' @examples
+#' \dontrun{
+#' entity <- Entity$new(id = uuid::UUIDgenerate())
+#' entity$uid
+#' entity$command()
+#' entity$query()
+#' }
+Entity <- R6::R6Class("Entity", public = list(
+    #' @field  Entity unique identifier.
+    uid = character(0),
+    #' @description
+    #' Instantiate Entity object.
+    #' @param uid (`character`) Entity unique identifier.
+    initialize = function(uid){
+        self$uid <- uid
+    }
+))
+
+# Public Methods ----------------------------------------------------------
+Entity$set(which = "public", overwrite = TRUE, name = "command", value = function(...){
+    # TODO: Write command code here
+    return(self)
+})
+
+Entity$set(which = "public", overwrite = TRUE, name = "query", value = function(){
+    # TODO: Write query code here
+    query_result <- mtcars
+    return(query_result)
+})
+
+
