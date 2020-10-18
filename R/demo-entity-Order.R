bf4a1a1941362c5eeb8e1c13ba5c0cda591f311e
harell <harel@loostiger.com>
2020-10-18 21:13:53 +1300

    degenerated add_pizza

--- R/demo-entity-Order.R
+++ R/demo-entity-Order.R
@@ -1,13 +1,23 @@
-# Order -------------------------------------------------------------------
-Order <- R6::R6Class("Order", inherit = Entity, lock_objects = FALSE)
+#' @title Order
+#' @family Pizza Ordering
+#' @noRd
+Order <- R6::R6Class("Order", inherit = Entity, lock_objects = FALSE, cloneable = FALSE)
 
-Order$set("public", "items", R6DS::RSet$new(equal = function(x, y) return(x$uid == y$uid)))
+# Public Methods ----------------------------------------------------------
+Order$set("public", "initialize", overwrite = TRUE, function(uid){
+    super$initialize(uid)
+    self$items <- R6DS::RSet$new(equal = function(x, y) return(x$uid == y$uid))
+    return(self)
+})
 
-# Order$set("public", "add_pizza", function(Pizza){
-#     stopifnot("Pizza" %in% class(Pizza))
-#     self$items$add(Pizza)
-#     return(self)
-# })
+Order$set("public", "add_pizza", function(Pizza){
+    stopifnot("Pizza" %in% class(Pizza))
+
+    # do.call(self$items$delete, args = list(val = Pizza))
+    # do.call(self$items$add, args = list(val = Pizza))
+
+    return(self)
+})
 
 # Order$set("public", "review_order", function(){
 # return(self$items$to)
--- tests/testthat/test-demo-Order.R
+++ tests/testthat/test-demo-Order.R
@@ -4,12 +4,19 @@ context("unit test for entity Order")
 testthat::setup({
     assign("test_env", testthat::test_env(), envir = parent.frame())
     test_env$order_uid <- "910e87ed-7565-4dc3-bfba-8aa072852f8d"
+    test_env$pizza <- Pizza$new(uid = "cc1d4f32")$add_topping("olives", "both")
 })
 
 # General -----------------------------------------------------------------
 test_that("calling Order$new instantiates an object with unique id", {
     attach(test_env)
-    expect_is(order <- Order$new(uid = order_uid), "Order")
-    expect_identical(order$uid, order_uid)
-    test_env$order <- order
+    expect_is(pizza_order <- Order$new(uid = order_uid), "Order")
+    expect_identical(pizza_order$uid, order_uid)
+    test_env$pizza_order <- pizza_order
+})
+
+# Public Methods ----------------------------------------------------------
+test_that('calling Order$add_pizza changes the state of Order', {
+    attach(test_env)
+    expect_is(pizza_order$add_pizza(pizza), "Order")
 })
