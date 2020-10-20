3148ee27965466ac4029dc3e583868eaf6b58294
harell <harel@loostiger.com>
2020-10-20 22:00:46 +1300

    updated docs

--- data-raw/event_storming.R
+++ data-raw/event_storming.R
@@ -1,19 +1,13 @@
 ## code to prepare `event_storming` dataset goes here
+event_storming <-
+    usethis::proj_path("data-raw", "event_storming", ext = "csv") %>%
+    readr::read_csv()
 
 event_storming <-
-    tibble::tribble(
-        ~Entity, ~Event,                 ~Command,             ~CQRS, ~Example,
-        "Order", "New order created",    "New",                "C",   "",
-        "Pizza", "New pizza created",    "New",                "C",   "",
-        "Order", "Pizza added to order", "Add pizza",          "C",   "",
-        "Pizza", "Pizza size selected",  "Select size",        "C",   "medium",
-        "Pizza", "Topping added",        "Add topping",        "C",   "olives",
-        "Pizza", "Topping added",        "Add topping",        "C",   "anchovy, but only on the right half",
-        "Pizza", "Topping removed",      "Remove topping",     "C",   "no olives",
-        "Pizza", "Pizza reviewed",       "Review",             "Q",   "",
-        "Order", "Order reviewed",       "Review",             "Q",   "",
-        "Order", "Order confirmed",      "Confirm",            "C",   ""
-    )
-
+    event_storming %>%
+    tidyr::replace_na(structure(rep("", ncol(event_storming)), .Names = colnames(event_storming)) %>% as.list())
 
 usethis::use_data(event_storming, overwrite = TRUE)
+
+
+
