4c21feb14a061ba56689b47031759f1d6f7470d6
harell <harel@loostiger.com>
2020-10-31 16:47:57 +1300

    update conventions

--- R/ddd-conventions.R
+++ R/ddd-conventions.R
@@ -10,8 +10,10 @@
 title <- new.env()
 
 title$event <- snakecase::to_upper_camel_case
+title$value <- snakecase::to_upper_camel_case
+title$entity <- snakecase::to_upper_camel_case
+title$service <- snakecase::to_upper_camel_case
 title$handler <- snakecase::to_snake_case
 title$command <- snakecase::to_snake_case
-title$entity <- snakecase::to_upper_camel_case
 title$domain <-  snakecase::to_title_case
 title$exammple <- snakecase::to_sentence_case
