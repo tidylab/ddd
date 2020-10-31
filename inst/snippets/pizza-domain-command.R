1e6b2d4a43c038e22993ccb3bb4223ed6ae80191
harell <harel@loostiger.com>
2020-10-31 14:07:38 +1300

    workflow and scenario

--- inst/snippets/pizza-domain-command.R
+++ inst/snippets/pizza-domain-command.R
@@ -5,15 +5,3 @@ add_step("add_topping", domain = "pizza_ordering")
 add_step("remove_topping", domain = "pizza_ordering")
 add_step("review_pizza", domain = "pizza_ordering")
 add_step("confirm_order", domain = "pizza_ordering")
-
-# Workflow ----------------------------------------------------------------
-session <- new.env()
-
-session %>%
-    start_order() %>%
-    add_pizza_to_order() %>%
-    select_pizza_size() %>%
-    add_topping() %>%
-    remove_topping() %>%
-    review_pizza() %>%
-    confirm_order()
