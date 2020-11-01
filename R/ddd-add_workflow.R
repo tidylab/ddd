49fb204b8ebe34df997610bd4ccd4da7390610ca
harell <harel@loostiger.com>
2020-11-01 18:10:24 +1300

    add workflow pattern doc

--- R/ddd-add_workflow.R
+++ R/ddd-add_workflow.R
@@ -3,6 +3,7 @@
 #' @param name (`character`) \code{Workflow} name.
 #' @param domain (`character`) sub-domain name.
 #' @param n_step (`character`) Number of steps that constitute the \code{Workflow}.
+#' @includeRmd vignettes/patterns/workflow.Rmd
 #' @family domain driven design patterns
 #' @export
 add_workflow <- function(name, domain = "domain", n_steps = 3){
