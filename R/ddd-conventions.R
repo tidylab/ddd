279d58d4fb8430c94b2b258eec27f38bec7ce4ca
harell <harel@loostiger.com>
2020-11-01 18:31:57 +1300

    standardize workflow name

--- R/ddd-conventions.R
+++ R/ddd-conventions.R
@@ -16,7 +16,7 @@ title$handler <- snakecase::to_snake_case
 title$command <- snakecase::to_snake_case
 title$domain <-  snakecase::to_title_case
 title$exammple <- snakecase::to_sentence_case
-
+title$workflow <- purrr::partial(snakecase::to_snake_case, sep_out = "-")
 
 # File Names --------------------------------------------------------------
 filename <- new.env()
@@ -25,30 +25,38 @@ filename$template <- function(entity = NULL, attribute = NULL, value = NULL){
     # NULL protection
     `%|>|%` <- function(a, b = identity){ if(is.null(a)) return(NULL) else return(b(a)) }
 
+
     paste0(paste(
         entity %|>|% snakecase::to_snake_case,
         attribute %|>|% snakecase::to_snake_case,
         value,
         sep = "-"
-    ), ".R")
+    ), ".R") %>%
+        stringr::str_replace_all("--", "-") %>%
+        stringr::str_remove_all("^-")
 }
 
-filename$event <- function(domain = "domain", name = "unnamed"){
+filename$event <- function(name = "unnamed", domain = "domain"){
     filename$template(domain, "event", title$event(name))
 }
 
-filename$value <- function(domain = "domain", name = "unnamed"){
+filename$value <- function(name = "unnamed", domain = "domain"){
     filename$template(domain, "value", title$value(name))
 }
 
-filename$entity <- function(domain = "domain", name = "unnamed"){
+filename$entity <- function(name = "unnamed", domain = "domain"){
     filename$template(domain, "entity", title$entity(name))
 }
 
-filename$service <- function(domain = "domain", name = "unnamed"){
+filename$service <- function(name = "unnamed", domain = "domain"){
     filename$template(domain, "service", title$service(name))
 }
 
-filename$command <- function(domain = "domain", name = "unnamed"){
+filename$command <- function(name = "unnamed", domain = "domain"){
     filename$template(domain, "command", title$command(name))
 }
+
+filename$workflow <- function(name = "unnamed", domain = "domain"){
+    filename$template(NULL, NULL, title$workflow(name))
+}
+
--- tests/testthat/test-ddd-add_workflow.R
+++ tests/testthat/test-ddd-add_workflow.R
@@ -8,8 +8,11 @@ testthat::setup({
 # Create R script ---------------------------------------------------------
 test_that("create R script without unit test", {
     attach(test_env)
+    name <- "order one pizza"
+    domain <- "pizza ordering"
+    path <- file.path(getwd(), "inst", "workflows", filename$workflow(name, domain))
     expect_silent(add_workflow(name, domain, n_steps = 3))
-
+    # expect_file_exists(path)
     # path <- proj_path("R", domain %+% "-" %+% name, ext = "R")
     # expect_true(file.exists(path))
     # unlink(path)
