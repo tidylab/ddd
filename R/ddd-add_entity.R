2fc20bda392d323c63a54c3a43fbb81a8e89f154
harell <harel@loostiger.com>
2020-11-05 20:52:45 +1300

    extract functions to .add_entity$generate_R_script

--- R/ddd-add_entity.R
+++ R/ddd-add_entity.R
@@ -10,6 +10,7 @@
 #' @family domain driven design patterns
 #' @export
 add_entity <- function(name, domain = NULL, commands = NULL, queries = NULL, testthat_exemption = FALSE, covr_exemption = testthat_exemption){
+    # Defensive Programming ---------------------------------------------------
     assert$is_character(name)
     assert$is_character(domain)
     assert$are_character(commands)
@@ -17,29 +18,8 @@ add_entity <- function(name, domain = NULL, commands = NULL, queries = NULL, tes
     assert$is_logical(testthat_exemption)
     assert$is_logical(covr_exemption)
 
-    add_entity_head <- function(name, domain, commands, queries){
-        template <- read_lines(find.template("templates", "entity", "head.R"))
-        str_glue(template, name = title$entity(name), domain = title$domain(domain))
-    }
-
-    add_entity_commands <- function(name, domain, commands, queries){
-        if(is.null(commands)) return()
-        template <- read_lines(find.template("templates", "entity", "commands.R"))
-        purrr::map_chr(commands, ~str_glue(template, name = title$entity(name), command = title$command(.x)))
-    }
-
-    add_entity_queries <- function(name, domain, commands, queries){
-        if(is.null(queries)) return()
-        template <- read_lines(find.template("templates", "entity", "queries.R"))
-        purrr::map_chr(queries, ~str_glue(template, name = title$entity(name), query = title$command(.x)))
-    }
-
-    excerpts <- list()
-    excerpts$head <- add_entity_head(name, domain, commands, queries)
-    excerpts$commands <- add_entity_commands(name, domain, commands, queries)
-    excerpts$queries <- add_entity_queries(name, domain, commands, queries)
-
     # Export Script -----------------------------------------------------------
+    excerpts <- .add_entity$generate_R_script(name, domain, commands, queries)
     file_path <- file.path(getwd(), "R", filename$entity(name, domain))
     file.create(file_path)
 
@@ -50,4 +30,34 @@ add_entity <- function(name, domain = NULL, commands = NULL, queries = NULL, tes
 
     if(interactive()) fs::file_show(file_path) # nocov
     invisible()
+}
+.add_entity <- new.env()
+
+
+# High-level functions ----------------------------------------------------
+.add_entity$generate_R_script <- function(name, domain, commands, queries){
+    excerpts <- list()
+    excerpts$head <- .add_entity$add_entity_head(name, domain, commands, queries)
+    excerpts$commands <- .add_entity$add_entity_commands(name, domain, commands, queries)
+    excerpts$queries <- .add_entity$add_entity_queries(name, domain, commands, queries)
+    return(excerpts)
+}
+
+
+# Low-level functions -----------------------------------------------------
+.add_entity$add_entity_head <- function(name, domain, commands, queries){
+    template <- read_lines(find.template("templates", "entity", "head.R"))
+    str_glue(template, name = title$entity(name), domain = title$domain(domain))
+}
+
+.add_entity$add_entity_commands <- function(name, domain, commands, queries){
+    if(is.null(commands)) return()
+    template <- read_lines(find.template("templates", "entity", "commands.R"))
+    purrr::map_chr(commands, ~str_glue(template, name = title$entity(name), command = title$command(.x)))
+}
+
+.add_entity$add_entity_queries <- function(name, domain, commands, queries){
+    if(is.null(queries)) return()
+    template <- read_lines(find.template("templates", "entity", "queries.R"))
+    purrr::map_chr(queries, ~str_glue(template, name = title$entity(name), query = title$command(.x)))
 }
