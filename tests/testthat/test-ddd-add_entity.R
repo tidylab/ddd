8115dc6b86fff7ba80910633bab92bb906c68a02
harell <harel@loostiger.com>
2020-11-08 12:17:37 +1300

    refactor tests

--- tests/testthat/test-ddd-add_entity.R
+++ tests/testthat/test-ddd-add_entity.R
@@ -5,7 +5,7 @@ testthat::setup({
     assign("test_env", testthat::test_env(), envir = parent.frame())
     withr::local_dir(test_wd, .local_envir = test_env)
     test_env$name <- "Pizza"
-    test_env$domain <- "pizza_ordering"
+    test_env$domain <- "Pizza Ordering"
     test_env$commands <- c("select_size", "add_topping")
     test_env$queries <- c("review")
 })
@@ -15,16 +15,15 @@ test_that("create an R script", {
     attach(test_env)
 
     expect_null(add_entity(name, domain, commands, queries))
-    file_name <- stringr::str_glue("{domain}-entity-{name}.R", name = name, domain = domain)
-    file_path <- file.path(getwd(), "R", file_name)
+    file_path <- file.path(getwd(), "R", filename$entity(name, domain))
     expect_file_exists(file_path)
 
     file_content <- readLines(file_path)
-    expect_match(file_content, "Pizza")
-    expect_match(file_content, "Pizza Ordering")
-    expect_match(file_content, "select_size")
-    expect_match(file_content, "add_topping")
-    expect_match(file_content, "review")
+    expect_match(file_content, name)
+    expect_match(file_content, domain)
+    expect_match(file_content, commands[1])
+    expect_match(file_content, commands[2])
+    expect_match(file_content, queries[1])
 })
 
 test_that("create abstract base class (ABC) script", {
@@ -34,4 +33,13 @@ test_that("create abstract base class (ABC) script", {
 
     file_content <- readLines(file_path)
     expect_match(file_content, "Entity")
+})
+
+test_that("create a unit-test", {
+    attach(test_env)
+    file_path <- file.path(getwd(), "tests", "testthat", paste0("test-", filename$entity(name, domain)))
+    # expect_file_exists(file_path)
+
+    # file_content <- readLines(file_path)
+    # expect_match(file_content, "Entity")
 })
