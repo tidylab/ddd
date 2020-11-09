cc27258ab23105c40f8382cbb887832bf1abb771
harell <harel@loostiger.com>
2020-11-09 20:46:58 +1300

    short description

--- inst/templates/value-object/template.R
+++ inst/templates/value-object/template.R
@@ -7,18 +7,18 @@
 {name} <- function(
     size = NA_character_,
     slices = NA_integer_,
-    cheese = NA,
-    toppings = list()
+    toppings = list(),
+    takeaway = NA
 ){{
     stopifnot(is.character(size))
     stopifnot(is.integer(slices), slices > 0, slices <= 8)
-    stopifnot(is.character(cheese))
     stopifnot(is.list(toppings))
+    stopifnot(is.logical(takeaway))
 
     tibble::tibble(
         size = size,
         slices = slices,
-        cheese = cheese,
-        toppings = toppings
+        toppings = toppings,
+        takeaway = takeaway
     )
 }}
