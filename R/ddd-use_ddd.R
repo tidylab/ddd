1c861fba325b4ccb0c72bd7d853700bfd9d56537
harell <harel@loostiger.com>
2020-12-02 19:15:56 +1300

    updated doc

--- R/ddd-use_ddd.R
+++ R/ddd-use_ddd.R
@@ -1,6 +1,9 @@
 #' @title Make a Project Independent of the \code{ddd} Package
 #' @param path (`character`) Path to the root folder of the project.
 #' @includeRmd vignettes/articles/use_ddd.Rmd
+#' @section Workflow:
+#' 1. Call \code{\link{use_ddd}} maximum once per project; this will make the project independent of `ddd`
+#' 2. Call \code{devtools::document} (or Ctrl+Shift+D on windows) to include the copied classes in the project NAMESPACE file
 #' @export
 use_ddd <- function(path = "."){
     fs::dir_exists(path)
